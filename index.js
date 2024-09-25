9c7556be632ac916b2a4c77f00e38c8bc38fd9ad
fadouma ahmed <fannyahmed85@gmail.com>
2024-09-25 12:23:30 -0600

    complete

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    if (someValue > 42 ){ 
+        return someValue - 42
+ } else if ( someValue < 42) {
+    return 42 - someValue
+ }
+}
+function distanceFromHqInFeet(someValue) {
+    if (someValue > 42){
+        return (someValue - 42) * 264
+    } else if (someValue < 42){
+        return (42 - someValue) * 264
+    }
+}
+function distanceTravelledInFeet(start,destination){
+if (start > destination){
+    return (start - destination) * 264
+} else if (start < destination){
+    return (destination - start) * 264
+}
+}
+function calculatesFarePrice(start,destination){
+    let feet = distanceTravelledInFeet(start,destination)
+    if (feet < 400){
+        return 0
+    } if (feet >= 400 && feet <= 2000){
+        return (feet - 400) * .02
+    } else if (feet > 2000 && feet < 2500){
+        return 25
+    } else if (feet > 2500){
+        return "cannot travel that far"
+    }
+}
